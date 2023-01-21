8302ded617f29d8c73cdc22f7ab9cc66e05b6c4c
Pattie Goch <patrycjagoch@gmail.com>
2023-01-21 17:37:43 +0100

    made a set of data for colleagues

---
+++ arrays/arrays_zadanie.js
@@ -0,0 +1,54 @@
+// //const data = [
+//     {
+//         name: 'Shrek',
+//         age: 106,
+//         offsprings: 3,
+//         city: 'the Swamp',
+//         hobbies: ['swamp', 'eating']
+//     },
+//
+//     {
+//         name: 'Fiona',
+//         age: 23,
+//         offsprings: 3,
+//         city: 'the Swamp',
+//         hobbies: ['feminism', 'shrek']
+//     },
+//
+//     {
+//         name: 'Donkey',
+//         age: 22,
+//         offsprings: 5,
+//         city: 'Far Far Away',
+//         hobbies: ['dragons', 'shrek']
+//     },
+//     {
+//         name: 'Dragon',
+//         age: 1250,
+//         offsprings: 5,
+//         city: 'Far Far Away',
+//         hobbies: ['makeup', 'towers']
+//
+//     },
+//     {
+//         name: 'Lord Farquaad',
+//         age: 571,
+//         offsprings: 0,
+//         city: 'Duloc',
+//         hobbies: ['power', 'mirrors']
+//
+//     },
+//     {
+//         name: 'Puss in Boots',
+//         age: 20,
+//         offsprings: 3,
+//         city: 'Far Far Away',
+//         hobbies: ['gazpacho', 'working']
+//     }
+// ]
+//
+//
+// 1. Podaj imiona postaci z najwieksza liczba dzieci
+// 2. Podaj imie pradawnego najstarszego stworzenia
+// 3. Podaj imie kogos z unikatowym miastem zamieszkania
+// 4. Podaj imiona postaci, ktore kochaja Shreka [hobby]
