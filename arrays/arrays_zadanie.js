aa830e13416eb9fe013c4705a8ecc2623e85ea82
Pattie Goch <patrycjagoch@gmail.com>
2023-01-23 22:06:55 +0000

    Update arrays_zadanie.js
    Resolved

--- arrays/arrays_zadanie.js
+++ arrays/arrays_zadanie.js
@@ -52,3 +52,27 @@
 // 2. Podaj imie pradawnego najstarszego stworzenia
 // 3. Podaj imie kogos z unikatowym miastem zamieszkania
 // 4. Podaj imiona postaci, ktore kochaja Shreka [hobby]
+
+function hasMostOffspring(data) {
+
+    const sorted = data.sort((char1, char2) => char2.offsprings - char1.offsprings);
+
+    return sorted.filter(({offsprings}) => offsprings === sorted.at(0).offsprings).map(({name}) => name);
+}
+
+function whoseOldest(data) {
+
+    const sorted = data.sort((char1, char2) => char2.age - char1.age);
+    return sorted.at(0).name;
+}
+
+function whoLovesShrek(data) {
+
+    shrekLovers = data.filter(({hobbies}) => {
+        return hobbies
+            .map((hobby) => hobby.toLowerCase())
+            .includes('shrek')
+    })
+
+    return shrekLovers.map(({name})=> name);
+}
